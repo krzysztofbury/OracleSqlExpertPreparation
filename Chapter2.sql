e9e4b1f03fd4f7cd4a2405109bd595f962f16ee0
Krzysztof Bury <krzysiek.bury@gmail.com>
2015-01-04 00:38:15 +0100

    Chapter 2 finished.

--- Chapter2.sql
+++ Chapter2.sql
@@ -8,3 +8,24 @@ CREATE TABLE work_schedule (
 work_schedule_id NUMBER,
 start_date		 DATE,
 end_date		 DATE);
+
+/* TESTING OUT NAMING CONVENTIONS AND DOUBLE QUOTING */
+CREATE TABLE "test" (
+a 	NUMBER,
+"b" VARCHAR(20));
+
+CREATE TABLE cruises (
+cruise_id	NUMBER,
+cruise_type_id NUMBER,
+cruise_name VARCHAR2(20),
+captain_id NUMBER NOT NULL,
+start_date	DATE,
+end_date	DATE,
+status	VARCHAR2(5) DEFAULT 'DOCK',
+CONSTRAINT cruise_pk PRIMARY KEY (cruise_id));
+
+CREATE TABLE cruise_notes (
+cruise_notes_id NUMBER,
+cruise_notes	CLOB);
+
+
