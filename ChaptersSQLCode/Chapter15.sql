8490d40c41de7417b3841af43917888a3762d3fc
Krzysztof Bury <krzysiek.bury@gmail.com>
2015-03-24 00:25:13 +0100

    Added notes after re-reading chapter 15.

--- ChaptersSQLCode/Chapter15.sql
+++ ChaptersSQLCode/Chapter15.sql
@@ -21,6 +21,21 @@ INSERT option
 SUBQUERY;
 
 -- option is one of two: ALL or FIRST
--- * ALL is the default and may be omitted
+-- * ALL is the default and may be omitted, but only when WHEN expression is present, otherwise it will rise a syntax error
 -- * FIRST indicates that the only set of INTO clauses that will execute are those that follow the first WHEN clause that evaluates to TRUE
 -- each WHEN condition is followed by one or more INTO clauses
+
+/* MERGE STATEMENT */
+MERGE INTO table
+USING table / view / subquery
+ON condition
+WHEN MATCHED THEN UPDATE SET col = expression / DEFAULT
+		where_clause
+		DELETE where_clause
+WHEN NOT MATCHED THEN INSERT (col, col2)
+		VALUES (expr1, expr2 / DEFAULT)
+		where_clause
+WHERE condition;
+
+-- DELETE clause only affects rows that are a result of the completed "UPDATE clause" and remain in the target table - which in this instance is WWA_INVOICES
+
